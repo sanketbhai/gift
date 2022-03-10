5555e122b157ec4fdbac04338797e023ce4d064e
sanketbhai <gsanket0903@gmail.com>
2022-03-10 13:00:14 +0530

    error resolved

--- birthdaygift/settings.py
+++ birthdaygift/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1o_7+@2gzeonlvpu8wt3er@f!dqh@wrj((f!zd+$f2y(=(zqw&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['<url>']
+ALLOWED_HOSTS = []
 
 
 # Application definition
--- gift/urls.py
+++ gift/urls.py
@@ -2,7 +2,7 @@ from django.urls import include, path
 from . import views
 urlpatterns = [
 
-    path('', views.index,name="index"),
+    # path('', views.index,name="index"),
     path('<slug:sender>/<slug:receiver>/', views.gotolink,name="gotolink"),
     path('generatelink/', views.generatelink,name="generatelink"),
 
